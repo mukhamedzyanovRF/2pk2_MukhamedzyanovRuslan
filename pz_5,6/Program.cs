567e8997ccf1345cab22d9cf4b2c74ed1e7f3cd7
крокодил <@DESKTOP-QA6NDR1>
2022-09-29 18:03:39 +0500

    Добавьте файлы проекта.

---
+++ pz_5,6/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace pz_5_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        //{
+        //    int a;
+        //    for (a = 10; a <= 80; a += 4)
+        //    Console.WriteLine(a);
+        //}
+        //{
+        //    char f;
+        //    for (f = 'В'; f != 'Й'; f++)
+        //        Console.Write("{0}", f);  
+        //}
+        //{
+        //    int n;
+        //    int m;
+        //    for(n = 0; n<7;n++)
+        //    {
+        //        for (m = 0; m < 7; m++)
+        //            Console.Write('#');
+        //        Console.WriteLine('\n');
+        //    }
+        //}
+        //pz_6
+        //{
+        //    double a = 1.45;
+        //    double b = 2.3;
+        //    double x;
+        //    double y;
+        //    for (x = 0; x <= 1; x += 0.1)
+        //    {
+        //        y = x + b * x * Math.Sin(a);
+        //        Console.WriteLine(string.Format("{0} \t {1}", x, y));
+        //        Console.WriteLine();
+        //    }
+        //}
+    }
+}
